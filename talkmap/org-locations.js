188f7c8211abcff7f6e38aaf247028aea67822fc
Ivan Cucchi <159425285+ivanqkk@users.noreply.github.com>
2026-09-01 17:55:12 +0200

    Update GitHub Actions workflow and JavaScript location data; upgrade action versions and Python, modify talk locations for accuracy

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,13 +1,18 @@
 var addressPoints = [
   [
-    "Caff\u00e9 Beltrami<br />University of Pavia, Aula Beltrami; Pavia, Italy",
-    45.0368547,
+    "Spring Workshop COMPMAT 2025<br />University of Pavia, Aula Grande; Pavia, Italy",
+    45.0368546,
     9.1378251
   ],
   [
-    "5th Edition of Young Applied Mathematicians Conference<br />University of Padua, Aula Magna; Padua, Italy",
-    45.3984429,
-    11.8956829
+    "ICAM Workshop<br />University of Pavia, Aula Grande; Pavia, Italy",
+    45.0368546,
+    9.1378251
+  ],
+  [
+    "Caff\u00e8 Beltrami Seminars<br />University of Pavia, Aula Beltrami; Pavia, Italy",
+    45.0368546,
+    9.1378251
   ],
   [
     "1st Workshop on Data Science for Health and Biology<br />Politecnico di Milano, Campus Leonardo; Milan, Italy",
@@ -15,28 +20,23 @@ var addressPoints = [
     9.1896346
   ],
   [
-    "Spring Workshop COMPMAT 2025<br />University of Pavia, Aula Grande; Pavia, Italy",
-    45.0368547,
+    "Scientific Machine Learning, Emerging Topics<br />SISSA - International School for Advanced Studies, Main Campus; Trieste, Italy",
+    45.6496485,
+    13.7772781
+  ],
+  [
+    "Spring Workshop COMPMAT 2024<br />University of Pavia, Aula Magna; Pavia, Italy",
+    45.0368546,
     9.1378251
   ],
   [
-    "Scientific Machine Learning, emerging topics<br />SISSA - International School for Advanced Studies, Main Campus; Trieste, Italy",
-    45.6496485,
-    13.7772781
+    "5th Edition of Young Applied Mathematicians Conference<br />University of Padua, Aula Magna; Padua, Italy",
+    45.3984428,
+    11.8956829
   ],
   [
     "3rd Workshop of UMI Group \u2014 Mathematics for Artificial Intelligence and Machine Learning<br />University of Bari, Aula Magna; Bari, Italy",
     41.1257843,
     16.8620293
-  ],
-  [
-    "Spring Workshop COMPMAT 2024<br />University of Pavia, Aula Magna; Pavia, Italy",
-    45.0368547,
-    9.1378251
-  ],
-  [
-    "ICAM Workshop<br />University of Pavia, Aula Grande; Pavia, Italy",
-    45.0368547,
-    9.1378251
   ]
 ];
